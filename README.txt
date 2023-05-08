b6a9ae2d7e833039b9e3f2c0fa4b7624273777ad
JohannTP <Johannccn@gmail.com>
2023-05-08 18:11:28 -0500

    v0-08-2023

---
+++ README.txt
@@ -0,0 +1,15 @@
+
+-Challence I es un proyecto de diseño de un sitio web que sirve para encriptar y desencriptar mensajes siguiendo las siguientes reglas:
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" 
+
+-Este proyecto ah sido desarrollado usando las siguientes tecnologias de desarrollo:
+1. HTML 5
+2. CSS
+3. JavaScript
+
+-Este proyecto ha sido desarrollado con la finalidad de poner en practica mis conocimientos de desarollo obtenidos en la Etapa de Seleccion del programa Oracle Next Education el cual es un programa que imparte Oracle junto con la plataforma de aprendizaje Alura Latam de la cual formo parte del Grupo 5 (G5)...
---
+++ script.js
@@ -0,0 +1,50 @@
+/* La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+/*
+Ejemplo
+"gato" => "gaitober"
+gaitober" => "gato"*/
+
+function encriptar(){
+   var texto = document.getElementById("inputTexto").value.toLowerCase();
+
+   var txtCifrado = texto.replace(/e/igm,"enter");
+   var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+   var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+   var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+   var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+
+   document.getElementById("imgright").style.display = "none";
+   document.getElementById("texto").style.display = "none";
+   document.getElementById("texto2").innerHTML = txtCifrado;
+   document.getElementById("copiar").style.display = "show";
+   document.getElementById("copiar").style.display = "inherit";
+
+}
+
+function desencriptar(){
+  var texto = document.getElementById("inputTexto").value.toLowerCase();
+
+  var txtCifrado = texto.replace(/enter/igm,"e");
+  var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+  var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+  var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+  var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+
+  document.getElementById("imgright").style.display = "none";
+  document.getElementById("texto").style.display = "none";
+  document.getElementById("texto2").innerHTML = txtCifrado;
+  document.getElementById("copiar").style.display = "show";
+  document.getElementById("copiar").style.display = "inherit";
+
+}
+function copiar(){
+  var contenido = document.querySelector("#texto2");
+  contenido.select();
+  document.execCommand("copy");
+  alert("¡Se copió!")
+}
